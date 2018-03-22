1bab3a0930ad348b366ac10d96a4b682277c289d
张广森 <sp6688@vip.qq.com>
2018-03-22 22:16:22 +0800

    1

--- bitframework/configs/util.js
+++ bitframework/configs/util.js
@@ -9,10 +9,14 @@ let util={
 			var keyPair = bitcoin.ECPair.makeRandom({ network: testnet, rng: rng })
     		address = keyPair.getAddress()
 		}else{
-			let hash = bitcoin.crypto.sha256(Buffer.from(openid))
-			let d = bigi.fromBuffer(hash)
-			let keyPair = new bitcoin.ECPair(d)
-			address = keyPair.getAddress()
+			function rng () { return Buffer.from(openid) }
+			let testnet = bitcoin.networks.testnet
+			var keyPair = bitcoin.ECPair.makeRandom({ network: testnet, rng: rng })
+    		address = keyPair.getAddress()
+			// let hash = bitcoin.crypto.sha256(Buffer.from(openid))
+			// let d = bigi.fromBuffer(hash)
+			// let keyPair = new bitcoin.ECPair(d)
+			// address = keyPair.getAddress()
 		}
 		return address;
 	},
